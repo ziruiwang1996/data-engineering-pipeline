56d0ee9eeb1b28d791587b6894ed2538154fbfd7
zirui <wangzr926@gmail.com>
2025-05-30 23:24:10 -0400

    latest

--- airflow/dags/pipeline_dag.py
+++ airflow/dags/pipeline_dag.py
@@ -1,8 +1,16 @@
+import os
 from datetime import datetime, timedelta
 from airflow import DAG
 from docker.types import Mount
 from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from airflow.providers.docker.operators.docker import DockerOperator
+
+# Set AWS/MinIO credentials as environment variables
+os.environ['AWS_ACCESS_KEY_ID'] = 'minioadmin'
+os.environ['AWS_SECRET_ACCESS_KEY'] = 'minioadmin'
+os.environ['AWS_ENDPOINT_URL'] = 'http://minio:9000'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PYSPARK_PATH = os.path.join(BASE_DIR, "../../pyspark")
 
 default_args = {
     'owner': 'admin',
@@ -15,15 +23,16 @@ with DAG(
     dag_id="etl",
     default_args=default_args,
     schedule="@daily",
-    start_date=datetime(2025, 5, 28),
-    catchup=False,
+    start_date=datetime(2025, 5, 30),
+    catchup=False
 ) as dag:
     
     t1 = S3KeySensor(
         task_id="wait_for_raw_csv",
         bucket_key="raw-data/*.csv",
         bucket_name="raw-data",
-        aws_conn_id="minio_default",
+        aws_conn_id=None,  # Use environment variables instead
+        verify=False,
         poke_interval=60,
         timeout=60*60,
         mode="poke"
@@ -45,8 +54,8 @@ with DAG(
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         mounts=[
-            Mount(source='minio_data', target='/mnt/minio', type='bind'),   # mount MinIO data for shared access
-            Mount(source='pyspark', target='/app', type='bind'),            # map local directory to the container
+            Mount(source="minio_data", target="/mnt/minio", type="volume"),
+            Mount(source=PYSPARK_PATH, target="/app", type="bind")
         ]
     )
 
